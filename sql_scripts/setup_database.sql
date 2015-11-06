33cabbe5deb2e4538f6641d372fe5244b32c29b6
schnip <pingthegreenflag@gmail.com>
2015-11-05 23:52:24 -0500

    Added all the tables, next up is the FK constraints

--- sql_scripts/setup_database.sql
+++ sql_scripts/setup_database.sql
@@ -23,3 +23,41 @@ recipe_id int,
 value int,
 constraint pk_username_recipe_id primary key (username, recipe_id)
 );
+
+create table if not exists ingredients(
+recipe_id int,
+i_num int not null auto_increment,
+quantity varchar(255),
+name varchar(255),
+constraint pk_recipe_ingredient primary key (recipe_id, i_num)
+);
+
+create table if not exists directions(
+recipe_id int,
+d_num int not null auto_increment,
+d_text varchar(4095),
+);
+
+create table if not exists gen_foods(
+name varchar(255),
+ara boolean,
+type varchar(255),
+primary key(name)
+);
+
+create table if not exists gen_steps(
+id int not null auto_increment,
+primary_action varchar(255),
+secondary_action varchar(255),
+result varchar(255),
+primary_type varchar(255),
+secondary_type varchar(255),
+result_type varchar(255),
+primary key(id)
+);
+
+create table if not exists gen_type(
+type varchar(255),
+parent varchar(255),
+primary key(type)
+);
