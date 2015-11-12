1776fc2b917ad81ae91c96938e0d0bf59e654aad
schnip <pingthegreenflag@gmail.com>
2015-11-12 11:40:56 -0500

    Worked more on get recipe

--- get_random_recipe.php
+++ get_random_recipe.php
@@ -1,3 +1,5 @@
 <?php
-$db = new PDO("mysql:dbname=imdb;host=localhost", "user", "test");
+$db = new PDO("mysql:dbname=imdb;host=localhost", "user", "t3st");
+$rows = $db->query("select * from recipes order by RAND() limit 1;")
+$rows = $db->query("select * from ingredients where recipe_id=" . $recipe_id . ';');
 ?>
