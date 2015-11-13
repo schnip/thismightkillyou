96941e9801f91c2ff082aec55d5c886cdaadf23a
schnip <pingthegreenflag@gmail.com>
2015-11-12 20:56:00 -0500

    made get random recipe not break

--- get_random_recipe.php
+++ get_random_recipe.php
@@ -1,6 +1,8 @@
 <?php
+// This won't work until we actually have some data, so next task is to write the helpers necessary
+
 $db = new PDO("mysql:dbname=imdb;host=localhost", "user", "t3st");
-$rows = $db->query("select * from recipes order by RAND() limit 1;")
-echo $rows
-$rows = $db->query("select * from ingredients where recipe_id=" . $recipe_id . ';');
+$rows = $db->query("select * from recipes order by RAND() limit 1;");
+echo $rows;
+//$rows = $db->query("select * from ingredients where recipe_id=" . $recipe_id . ';');
 ?>
