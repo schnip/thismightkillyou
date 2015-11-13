f6d28bc63d4471da3d03d9dd4b2b19765e257bca
schnip <pingthegreenflag@gmail.com>
2015-11-13 09:11:08 -0500

    Random step is getting there

--- get_random_step.php
+++ get_random_step.php
@@ -1,13 +1,31 @@
 <?php
 $db = new PDO("mysql:dbname=thismightkillyou;host=localhost", "user", "t3st");
 $query = "select * from gen_type";
-if (isset($_GET['parent'])) {
-	$query = $query . ' where parent = ' . $db->quote($_GET['parent']);
+if (isset($_GET['ara'])) {
+	$query = $query . ' where ';
+}
+if (isset($_GET['ara'])) {
+	$query = $query . ' ara = true ';
 }
 $query = $query . " order by RAND() limit 1;";
 
 $rows = $db->query($query);
 foreach ($rows as $row) {
-	echo $row['type'];
+	echo $row['id'];
+	echo "\n";
+	echo $row['primary_action'];
+	echo "\n";
+	echo $row['secondary_action'];
+	echo "\n";
+	echo $row['result'];
+	echo "\n";
+	echo $row['ara'];
+	echo "\n";
+	echo $row['primary_type'];
+	echo "\n";
+	echo $row['secondary_type'];
+	echo "\n";
+	echo $row['result_type'];
+	echo "\n";
 }
 ?>
