8b9ef7c47a0a1013757f4bb5b56394275017ea52
schnip <pingthegreenflag@gmail.com>
2015-11-13 09:48:17 -0500

    get steps now returns a json

--- get_random_step.php
+++ get_random_step.php
@@ -1,6 +1,6 @@
 <?php
 $db = new PDO("mysql:dbname=thismightkillyou;host=localhost", "user", "t3st");
-$query = "select * from gen_type";
+$query = "select * from gen_steps";
 if (isset($_GET['ara'])) {
 	$query = $query . ' where ';
 }
@@ -11,7 +11,17 @@ $query = $query . " order by RAND() limit 1;";
 
 $rows = $db->query($query);
 foreach ($rows as $row) {
-	echo $row['id'];
+	$data = array();
+	$data['id']=$row['id'];
+	$data['primary_action']=$row['primary_action'];
+	$data['secondary_action']=$row['secondary_action'];
+	$data['result']=$row['result'];
+	$data['ara']=$row['ara'];
+	$data['primary_type']=$row['primary_type'];
+	$data['secondary_type']=$row['secondary_type'];
+	$data['result_type']=$row['result_type'];
+	echo json_encode($data);
+/*	echo $row['id'];
 	echo "\n";
 	echo $row['primary_action'];
 	echo "\n";
@@ -26,6 +36,6 @@ foreach ($rows as $row) {
 	echo $row['secondary_type'];
 	echo "\n";
 	echo $row['result_type'];
-	echo "\n";
+	echo "\n";*/
 }
 ?>
