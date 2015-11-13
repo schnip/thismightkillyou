047e12e2522bff08213c17224709d6a9c3fe169f
schnip <pingthegreenflag@gmail.com>
2015-11-13 08:52:47 -0500

    added dummy file for getting random step

---
+++ get_random_step.php
@@ -0,0 +1,13 @@
+<?php
+$db = new PDO("mysql:dbname=thismightkillyou;host=localhost", "user", "t3st");
+$query = "select * from gen_type";
+if (isset($_GET['parent'])) {
+	$query = $query . ' where parent = ' . $db->quote($_GET['parent']);
+}
+$query = $query . " order by RAND() limit 1;";
+
+$rows = $db->query($query);
+foreach ($rows as $row) {
+	echo $row['type'];
+}
+?>
