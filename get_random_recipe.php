9d5e8779a65a74ad221105433ffcd15b609eb66a
schnip <pingthegreenflag@gmail.com>
2015-11-12 19:58:01 -0500

    Moved the home to the index because that makes the site show up nicely

--- get_random_recipe.php
+++ get_random_recipe.php
@@ -1,5 +1,6 @@
 <?php
 $db = new PDO("mysql:dbname=imdb;host=localhost", "user", "t3st");
 $rows = $db->query("select * from recipes order by RAND() limit 1;")
+echo $rows
 $rows = $db->query("select * from ingredients where recipe_id=" . $recipe_id . ';');
 ?>
