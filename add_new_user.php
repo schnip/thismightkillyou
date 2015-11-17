07ff0fb7e75bf1b6c01ef82f4735def7dd19af8a
schnip <pingthegreenflag@gmail.com>
2015-11-17 18:35:58 -0500

    Email is now optional for new users

--- add_new_user.php
+++ add_new_user.php
@@ -1,5 +1,9 @@
 <?php
 $db = new PDO("mysql:dbname=thismightkillyou;host=localhost", "user", "t3st");
-$query = 'insert into recipes (username, password, email) values (' . $db->quote($_GET['username']) . ',' . $db->quote($_GET['password']) . ',' . $db->quote($_GET['email']) . ');';
+$email = 'null';
+if (isset($_GET['email'])) {
+	$email = $db->quote($_GET['email']);
+}
+$query = 'insert into recipes (username, password, email) values (' . $db->quote($_GET['username']) . ',' . $db->quote($_GET['password']) . ',' . $email . ');';
 $db->exec($query);
 ?>
