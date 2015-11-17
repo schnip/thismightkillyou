6586ae992fc1b43d27711e3244bde3df8cc80f87
schnip <pingthegreenflag@gmail.com>
2015-11-16 21:04:27 -0500

    Food now can be variable size

--- get_random_food.php
+++ get_random_food.php
@@ -13,14 +13,25 @@ if ((isset($_GET['ara'])) && (isset($_GET['type']))) {
 if (isset($_GET['type'])) {
 	$query = $query . ' type = ' . $db->quote($_GET['type']);
 }
-$query = $query . " order by RAND() limit 1;";
+if (isset($_GET['num'])) {
+	$query = $query . " order by RAND() limit " . $_GET['num']) . ";";
+} else {
+	$query = $query . " order by RAND() limit 1;";
+}
 
 $rows = $db->query($query);
+$datas = array();
 foreach ($rows as $row) {
 	$data = array('name' => $row['name'], 'type' => $row['type']);
-	echo json_encode($data);
+	if (isset($_GET['num'])) {
+		array_push($datas, $data);
+	}
 /*	echo $row['name'];
 	echo "\n";
 	echo $row['type'];*/
 }
+if (isset($_GET['num'])) {
+	$data = array('names' => $datas);
+}
+echo json_encode($data);
 ?>
