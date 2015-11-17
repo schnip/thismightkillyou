d3f2dcd729622bd41efff97521888a0a7c7d4ab0
schnip <pingthegreenflag@gmail.com>
2015-11-16 22:32:12 -0500

    Hopefully that makes it happy

--- sql_scripts/ara-foods.php
+++ sql_scripts/ara-foods.php
@@ -3,18 +3,19 @@ $db = new PDO("mysql:dbname=thismightkillyou;host=localhost", "user", "t3st");
 $handle = fopen("ara-foods.txt", "r");
 $type = 'food';
 if ($handle) {
-    while (($line = fgets($handle)) !== false) {
-        if (substr($line,0,2) == '--') {
-        	$type = substr($line,2);
-        } elseif (strlen($line) < 2) {
+	while (($line = fgets($handle)) !== false) {
+		if (substr($line,0,2) == '--') {
+			$type = substr($line,2);
+		} elseif (strlen($line) < 2) {
 
-        } else {
-        	$query = 'insert into gen_food (name, ara, type) values (' . $line . ',true,' . $type . ');';
-        	$db->exec($query);
-        }
-    }
-    fclose($handle);
+		} else {
+			$query = 'insert into gen_food (name, ara, type) values (' . $line . ',true,' . $type . ');';
+			$db->exec($query);
+		}
+	}
+	fclose($handle);
 } else {
-    // error opening the file.
+	// error opening the file.
+	echo 'error opening file';
 } 
 ?>
