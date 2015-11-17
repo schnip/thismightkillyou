f7fa83ed3924d3c8dfaf6f5a75167058bb40846b
schnip <pingthegreenflag@gmail.com>
2015-11-16 19:24:16 -0500

    Wrong ordering

--- get_random_quantity.php
+++ get_random_quantity.php
@@ -3,7 +3,7 @@ $db = new PDO("mysql:dbname=thismightkillyou;host=localhost", "user", "t3st");
 $query = "select * from gen_quantities";
 if (isset($_GET['type'])) {
 	$query = $query . ' where type = ' . $db->quote($_GET['type']);
-	$query = $query . ' or type in (select type from gen_type where parent = ' . $db->quote($_GET['type']) . ')';
+	$query = $query . ' or type in (select parent from gen_type where type = ' . $db->quote($_GET['type']) . ')';
 	//$query = $query . ' or type in (select type from gen_type where parent in (select type from gen_type where parent in' . $db->quote($_GET['type']) . '))';
 }
 $query = $query . " order by RAND() limit 1;";
