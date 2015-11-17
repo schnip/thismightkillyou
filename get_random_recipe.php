8b2b4c11112f32ea404f1a1b2ada88d64833f4fc
schnip <pingthegreenflag@gmail.com>
2015-11-16 20:44:08 -0500

    Get random recipe should work now

--- get_random_recipe.php
+++ get_random_recipe.php
@@ -1,8 +1,27 @@
 <?php
-// This won't work until we actually have some data, so next task is to write the helpers necessary to create data
-
 $db = new PDO("mysql:dbname=thismightkillyou;host=localhost", "user", "t3st");
 $rows = $db->query("select * from recipes order by RAND() limit 1;");
-echo $rows;
-//$rows = $db->query("select * from ingredients where recipe_id=" . $recipe_id . ';');
+foreach ($rows as $row) {
+	$recipe_id = $row['id'];
+	$recipe_name = $row['name'];
+}
+$data = array();
+$data['id'] = $recipe_id;
+$data['name'] = $recipe_name;
+
+// Add in all the ingredients
+$rows = $db->query("select * from ingredients where recipe_id=" . $recipe_id . ';');
+$ingredients = array();
+foreach ($rows as $row) {
+	array_push($ingredients, array('name' => $row['name'], 'quantity' => $row['quantity']));
+}
+$data['ingredients'] = $ingredients;
+
+// Add in all the steps
+$rows = $db->query("select * from directions where recipe_id=" . $recipe_id . ';');
+$directions = array();
+foreach ($rows as $row) {
+	array_push($directions, $row['d_text']);
+}
+$data['directions'] = $directions;
 ?>
